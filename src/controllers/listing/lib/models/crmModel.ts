16101cb43968dc842ffa44dee9a472df155d6d1f
Saif Ur Rehman <s4saif.121@gmail.com>
2019-02-15 22:06:08 +0400

    #1 bills added

--- src/controllers/listing/lib/models/crmModel.ts
+++ src/controllers/listing/lib/models/crmModel.ts
@@ -1,13 +1,27 @@
 import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-
+const Billing = new Schema({
+    Product: String,
+    Price: Number,
+    Type: String,
+    Payed: Boolean
+});
+const Transaction = new Schema({
+    Product: String,
+    Price: Number,
+    AccountName: String,
+    Type: String,
+    Date: String
+});
 const AccountInfoSchema = new Schema({
     Activate: Boolean,
-    Deposit: Number,
+    Balance: Number,
     AccountName: String
-})
+});
 export const AccountSchema = new Schema({
     ID: Number,
-    Account: [AccountInfoSchema]
+    Account: [AccountInfoSchema],
+    Transaction:[Transaction],
+    Billing: [Billing]
 });
